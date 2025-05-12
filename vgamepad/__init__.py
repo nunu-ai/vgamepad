76dd9e5bcea330278271757348c8ea43e50c1741
nyanpyon <rorikidesu@gmail.com>
2025-05-12 14:33:05 +0200

    w

--- vgamepad/__init__.py
+++ vgamepad/__init__.py
@@ -1,7 +1,7 @@
 import sys
 from vgamepad.win.vigem_commons import VIGEM_TARGET_TYPE, XUSB_BUTTON, DS4_BUTTONS, DS4_SPECIAL_BUTTONS, DS4_DPAD_DIRECTIONS
 
-if sys.platform == 'windows':
+if sys.platform == 'win32':
     from vgamepad.win.virtual_gamepad import VX360Gamepad, VDS4Gamepad
 elif sys.platform == 'linux':  # Linux
     from vgamepad.lin.virtual_gamepad import VX360Gamepad, VDS4Gamepad
