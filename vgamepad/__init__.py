7c742eab55a426e9b9c1b7fe45b22d937fed7449
nyanpyon <rorikidesu@gmail.com>
2025-05-12 14:29:13 +0200

    Update __init__.py
    too much c

--- vgamepad/__init__.py
+++ vgamepad/__init__.py
@@ -1,7 +1,7 @@
-import platform
+import sys
 from vgamepad.win.vigem_commons import VIGEM_TARGET_TYPE, XUSB_BUTTON, DS4_BUTTONS, DS4_SPECIAL_BUTTONS, DS4_DPAD_DIRECTIONS
 
-if platform.system() == 'Windows':
+if sys.platform == 'windows':
     from vgamepad.win.virtual_gamepad import VX360Gamepad, VDS4Gamepad
-else if platform.system() == 'Linux':  # Linux
+elif sys.platform == 'linux':  # Linux
     from vgamepad.lin.virtual_gamepad import VX360Gamepad, VDS4Gamepad
